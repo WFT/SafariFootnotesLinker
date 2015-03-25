35b42813a316d9343d19ecf950ccaf8a37328551
WFT <will.a.ft@gmail.com>
2015-03-24 23:16:17 -0400

    initial commit

---
+++ Footnotes.safariextension/sup.js
@@ -0,0 +1,36 @@
+function FOOTNOTE_PROCESS() {
+    var sups = document.querySelectorAll("sup");
+    var prev_items = [];
+    var prev_text = [];
+
+    var is_linked = function(item) {
+	var ret = item.parentNode.tagName === "A";
+	var children = item.childNodes;
+	for (var i = 0, len = children.length; i < len; i++) {
+	    if (ret) break;
+	    ret = children[i].tagName === "A";
+	}
+	return ret;
+    };
+    
+    var link_item = function(item, name, href) {
+	item.innerHTML = '<a name="' + item.textContent + name +
+	    '" href="#' + item.textContent + href + '">' +
+	    item.innerHTML + '</a>';
+    };
+
+    for (var i = 0, len = sups.length; i < len; i++) {
+	var item = sups[i];
+	var prev_index = prev_text.indexOf(item.textContent);
+	if (prev_index == -1) {
+	    prev_items.push(item);
+	    prev_text.push(item.textContent);
+	} else if (!is_linked(item)) {
+	    var top = '-top';
+	    var bottom = '-bottom';
+	    link_item(item, bottom, top);
+	    link_item(prev_items[prev_index], top, bottom);
+	}
+    }
+}
+FOOTNOTE_PROCESS();
